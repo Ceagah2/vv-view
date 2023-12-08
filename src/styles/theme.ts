f3755f7f454d5e59b6eb0a8198f4552987aaf04a
Carlos Ceagah <carlosceagah@gmail.com>
2023-12-08 18:32:17 -0300

    feat: start initial config

---
+++ src/styles/theme.ts
@@ -0,0 +1,27 @@
+export const theme = {
+  colors: {
+    navy: '#192A56',
+    emerald: '#2ECC71',
+    sun: '#F1C40F',
+    white: '#FFFFFF',
+    gray: '#CCCCCC'
+  },
+  fonts: {
+    body: "'Roboto', sans-serif",
+    heading: "'Montserrat', sans-serif",
+    description: "'Open Sans', monospace",
+  },
+  sizes: {
+    heading: '2rem',
+    subheading: '1.5rem',
+    body: '1.25rem',
+    small: '1rem',
+  },
+  fontWeights: {
+    thin: 100,
+    light: 300,
+    normal: 400,
+    bold: 700,
+    black: 900,
+  }
+}
