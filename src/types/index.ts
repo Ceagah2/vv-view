3f83a78f450f83f7e271406ee0d04f4f7dd5abe8
Carlos Ceagah <carlosceagah@gmail.com>
2023-12-09 15:36:40 -0300

    type: create ISection interface

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,10 @@
 export interface IContainer {
   children: React.ReactNode
 }
+
+export interface ISection {
+  children: React.ReactNode
+  position: 'start' | 'end'
+  delay: number
+  title: string
+}
