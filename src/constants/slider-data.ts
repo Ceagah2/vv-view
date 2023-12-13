26a650b0b1fa9e0d2e8a59edc515e9ffa13499b6
Carlos Ceagah <carlosceagah@gmail.com>
2023-12-13 10:45:34 -0300

    refactor: update slider images

--- src/constants/slider-data.ts
+++ src/constants/slider-data.ts
@@ -2,12 +2,14 @@ import Capitolio from '../assets/captolio.webp'
 import Trilha from '../assets/trilha.jpg'
 import Praia from '../assets/praia.jpg'
 import Rota from '../assets/rota-do-vinhop.jpeg'
+import Cerveja from '../assets/cerveja.webp'
+import SaoThome from '../assets/sao-tome.jpeg'
 
 export const SliderData = [
   {
     id: 1,
     image: Praia,
-    title: 'Tilha das sete praias - Ubatuba/Sp',
+    title: 'Praias e Parques aquáticos',
   },
   {
     id: 2,
@@ -23,5 +25,15 @@ export const SliderData = [
     id: 4,
     image: Trilha,
     title: 'Trilhas diversas',
+  },
+  {
+    id: 5,
+    image: Cerveja,
+    title: 'Tour Cervejeiro',
+  },
+  {
+    id: 6,
+    image: SaoThome,
+    title: 'Sāo Thomé das Letras - MG',
   }
 ]
